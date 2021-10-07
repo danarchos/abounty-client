1ee53d59e87460e6b94e5e744876a38d74a6878f
Daniel McGrane <danielmcgrane@Daniels-MacBook-Pro.local>
2021-10-07 19:45:09 +0100

    remove unused imports

--- navigation/MainNavigator.tsx
+++ navigation/MainNavigator.tsx
@@ -1,6 +1,5 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 import Discover from "../screens/Discover/Discover.screen";
 import CreateBounty from "../screens/CreateBounty/CreateBounty.screen";
 import Rewards from "../screens/Rewards/Rewards.screen";
@@ -8,28 +7,23 @@ import theme from "../styles/theme/navigationTheme";
 
 import { observer } from "mobx-react-lite";
 import React, { FC, useEffect } from "react";
-import MenuButton from "../components/MenuButton";
 import navigationService, {
   mainLinking,
   MainNavigatorParamList,
   mainRoutes,
 } from "./NavigationService";
 import DrawerComponent from "../components/Drawer";
-import Button from "../components/common/Button";
 import { useSupabase } from "use-supabase";
-import { View } from "react-native";
 import useAuthPresenter from "../screens/authPresenter";
 import ViewBounty from "../screens/ViewBounty/ViewBounty";
 import ViewReward from "../screens/ViewReward/ViewReward";
-import LogoButton from "../components/LogoButton";
 
-const Stack = createStackNavigator<MainNavigatorParamList>();
 const Drawer = createDrawerNavigator<MainNavigatorParamList>();
 
 export const MainNavigator: FC = observer(() => {
   const { auth } = useSupabase();
 
-  const { performSignOut, setCurrentUser } = useAuthPresenter();
+  const { setCurrentUser } = useAuthPresenter();
 
   useEffect(() => {
     setCurrentUser(auth.user());
