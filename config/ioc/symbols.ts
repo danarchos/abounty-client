076105e6d61e78766597855f8705c520f6a31a5e
Daniel McGrane <danielmcgrane@Daniels-Mac-mini.local>
2021-10-21 08:31:51 +0100

    removed ioc redundant files

--- config/ioc/symbols.ts
+++
@@ -1,6 +0,0 @@
-// use SYMBOLS for binding anything else that is not a class
-// Classes are auto-bound
-export const SYMBOLS = {
-  SoldierFactory: Symbol.for('SoliderFactory'),
-  RootContainer: Symbol.for('RootContainer'),
-};
